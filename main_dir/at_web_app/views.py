14eb9be38b8e71032bb031c993f5a84ab65dab53
VadimAverinov <gwert2601@gmail.com>
2024-12-30 21:37:44 +0300

    add styles and finish profile.html

--- main_dir/at_web_app/views.py
+++ main_dir/at_web_app/views.py
@@ -2,18 +2,21 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from registration.forms import ProfileForm, RegistrationForm
 from django.contrib.auth import login
+from projects.models import Project
 
 @login_required
 def profile_view(request):
+    user = request.user
+    user_projects_num = len(Project.objects.filter(team__members__user=user))
     if request.method == 'POST':
-        form = ProfileForm(request.POST, request.FILES, instance=request.user)
+        form = ProfileForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
             form.save()
             return redirect('profile')
     else:
         form = ProfileForm(instance=request.user)
 
-    return render(request, 'registration/profile.html', {'form': form})
+    return render(request, 'registration/profile.html', {'form': form, 'user': user, 'user_projects_num': user_projects_num})
 
 def register_user(request):
     if request.method == 'POST':
--- main_dir/registration/forms.py
+++ main_dir/registration/forms.py
@@ -4,7 +4,7 @@ from projects.models import User
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['avatar', 'status', 'university_group']
+        fields = ['avatar', 'status']
 
 
 class RegistrationForm(forms.ModelForm):
